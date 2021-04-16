c5dd0cbf548259963cab88a446907d1109c9b036
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2021-04-16 13:12:02 +1200

    flip added!

--- ultimatecard.ts
+++ ultimatecard.ts
@@ -3,6 +3,7 @@ import { LitElement, html, css, property, customElement } from "lit-element";
 @customElement("ultimate-card")
 class UltimateCard extends LitElement {
   @property({ type: Boolean }) NonResponsive = false;
+  @property({ type: Boolean }) flipped = false;
 
   static get styles() {
     return css`
@@ -49,11 +50,41 @@ class UltimateCard extends LitElement {
         height: 100%;
         object-fit: contain;
       }
+
+      .image {
+        perspective: 800px;
+      }
+
+      .back {
+        background: red;
+        transform: rotateY(180deg);
+      }
+
+      .front,
+      .back {
+        backface-visibility: hidden;
+        grid-area: overlap;
+      }
+
+      .flip {
+        transform: rotateY(180deg);
+      }
+
+      .flipcontainer {
+        transform-style: preserve-3d;
+        grid-template-areas: "overlap";
+        display: grid;
+        transition: 1s all ease-in-out;
+      }
     `;
+  }
+  _handleImageClick() {
+    this.flipped = !this.flipped;
+    console.log("flip");
   }
   render() {
     return html`
-      <div class="${this.NonResponsive ? '' : 'CSSContainerQueryDiv'}">
+      <div class="${this.NonResponsive ? "" : "CSSContainerQueryDiv"}">
         <div class="container">
           <div class="TextArea">
             <div part="title" class="title">
@@ -64,8 +95,15 @@ class UltimateCard extends LitElement {
             </div>
           </div>
 
-          <div class="image">
-            <slot name="image">Image goes here!</slot>
+          <div class="image" @click="${this._handleImageClick}">
+            <div
+              class="${!this.flipped ? "flip flipcontainer " : "flipcontainer"}"
+            >
+              <div class="back"> <slot name="back">back text here</slot></div>
+              <div class="front">
+                <slot name="image">Image goes here!</slot>
+              </div>
+            </div>
           </div>
         </div>
       </div>
